6ba7aded86cc6b2d9a76814d00676ea22825999b
3hpyj <3hpyj@3h.net>
2023-11-28 11:50:56 +0800

    feat: 完成首页商品推荐crud

---
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/SimpleItemVO.java
@@ -0,0 +1,31 @@
+package com.imooc.pojo.vo;
+
+public class SimpleItemVO {
+    private String ItemId;
+    private String itemName;
+    private String itemUrl;
+
+    public String getItemId() {
+        return ItemId;
+    }
+
+    public void setItemId(String itemId) {
+        ItemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+}
