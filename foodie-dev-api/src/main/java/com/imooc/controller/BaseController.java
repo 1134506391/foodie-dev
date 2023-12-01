333c649e46a58cb11075dcd6aa021a11b5d7076f
3hpyj <3hpyj@3h.net>
2023-12-01 16:48:04 +0800

    feat: 完成头像上传，静态资源提供网络映射服务，图片上传大小限制

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -2,6 +2,8 @@ package com.imooc.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.io.File;
+
 @Controller
 public class BaseController {
     public static final String FOODIE_SHOPCART = "shopcart";
@@ -15,4 +17,11 @@ public class BaseController {
     // 微信支付成功 -> 支付中心 -> 天天吃货平台
     //                       |-> 回调通知的url
     String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+
+    // 用户上传头像的位置
+    public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
+            File.separator + "images" +
+            File.separator + "foodie" +
+            File.separator + "faces";
+//    public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
 }
