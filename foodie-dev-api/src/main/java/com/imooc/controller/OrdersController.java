78937679bd8638d0663d693382a553477296d971
3hpyj <3hpyj@3h.net>
2023-12-01 11:44:24 +0800

    feat: 定时任务关闭，超时未来支付订单

--- foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -1,7 +1,10 @@
 package com.imooc.controller;
 
+import com.imooc.enums.OrderStatusEnum;
 import com.imooc.enums.PayMethod;
+import com.imooc.pojo.OrderStatus;
 import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.pojo.vo.MerchantOrdersVO;
 import com.imooc.pojo.vo.OrderVO;
 import com.imooc.service.OrderService;
 import com.imooc.utils.IMOOCJSONResult;
@@ -10,10 +13,12 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +26,14 @@ import javax.servlet.http.HttpServletResponse;
 @Api(value = "订单相关", tags = {"订单相关的api接口"})
 @RequestMapping("orders")
 @RestController
-public class OrdersController {
+public class OrdersController extends BaseController {
     final static Logger logger = LoggerFactory.getLogger(OrdersController.class);
 
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
@@ -37,15 +44,15 @@ public class OrdersController {
             HttpServletResponse response) {
 
         if (submitOrderBO.getPayMethod() != PayMethod.WEIXIN.type
-                && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type ) {
+                && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type) {
             return IMOOCJSONResult.errorMsg("支付方式不支持！");
         }
 
-        System.out.println(submitOrderBO.toString());
+//        System.out.println(submitOrderBO.toString());
 
         // 1. 创建订单
-        orderService.createOrder(submitOrderBO);
-        return IMOOCJSONResult.ok();
+        OrderVO orderVO = orderService.createOrder(submitOrderBO);
+        String orderId = orderVO.getOrderId();
 
         // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
         /**
@@ -58,8 +65,42 @@ public class OrdersController {
 //        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
 
         // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
 
+        // 为了方便测试购买，所以所有的支付金额都统一改为1分钱
+        merchantOrdersVO.setAmount(1);
 
-//        return IMOOCJSONResult.ok();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf("aa"));
+        headers.add("imoocUserId","imooc");
+        headers.add("password","imooc");
+
+        HttpEntity<MerchantOrdersVO> entity =
+                new HttpEntity<>(merchantOrdersVO, headers);
+
+        ResponseEntity<IMOOCJSONResult> responseEntity =
+                restTemplate.postForEntity(paymentUrl,
+                        entity,
+                        IMOOCJSONResult.class);
+        IMOOCJSONResult paymentResult = responseEntity.getBody();
+        if (paymentResult.getStatus() != 200) {
+            logger.error("发送错误：{}", paymentResult.getMsg());
+            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
+
+        return IMOOCJSONResult.ok(orderId);
+    }
+    @PostMapping("notifyMerchantOrderPaid")
+    public Integer notifyMerchantOrderPaid(String merchantOrderId) {
+        orderService.updateOrderStatus(merchantOrderId, OrderStatusEnum.WAIT_DELIVER.type);
+        return HttpStatus.OK.value();
+    }
+
+    @PostMapping("getPaidOrderInfo")
+    public IMOOCJSONResult getPaidOrderInfo(String orderId) {
+
+        OrderStatus orderStatus = orderService.queryOrderStatusInfo(orderId);
+        return IMOOCJSONResult.ok(orderStatus);
     }
 }
